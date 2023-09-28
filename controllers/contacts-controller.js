9c223b550745de5b3770ff70ceba8da5b5077d53
Dmytro-Lys <d.lysenko122@gmail.com>
2023-09-28 15:38:32 +0300

    controllers and ctrlWrapper

---
+++ controllers/contacts-controller.js
@@ -0,0 +1,62 @@
+import { nanoid } from "nanoid";
+import * as contactService from "../models/contacts.js"
+import HttpError from "../helpers/HttpError.js";
+import cntrlWrapper from "../decorators/cntrlWrapper.js";
+
+const checkBody = ({ name = null, email = null, phone = null }) => {
+  return `${!name ? 'name': ''}${!email ? ' email': ''}${!phone ? ' phone': ''}`.trim().replaceAll(' ', ', ')
+}
+
+
+const getAll = async(req, res, next) => {
+     const contacts = await contactService.listContacts();
+    res.status(200).json({
+        contacts
+    })
+}
+
+const getById = async (req, res, next) => {
+    const { id } = req.params;
+    const contact = await contactService.getContactById(id)
+    if (!contact) throw HttpError(404, "Not found")
+    res.status(200).json({
+      contact 
+    }) 
+}
+
+const add =  async (req, res, next) => {
+    const messageBody = checkBody(req.body)
+    if (messageBody.length>0) throw HttpError (400, `missing required ${messageBody} field`)
+    const contact = await contactService.addContact({ id: nanoid(), ...req.body })
+    res.status(201).json({
+      contact 
+    });
+}
+
+const deleteById = async (req, res, next) => {
+     const { id } = req.params;
+    const contact = await contactService.removeContact(id)
+    if (!contact) throw HttpError(404, "Not found");
+    res.status(200).json({
+      message: "contact deleted",
+       contact
+    })
+ }
+
+const updateById = async (req, res, next) => {
+    if (Object.keys(req.body).length === 0) throw HttpError(400, `missing fields`);
+    const { id } = req.params;
+    const contact = await contactService.updateContact(id, req.body);
+    if (!contact) throw HttpError(404, "Not found");
+    res.status(200).json({
+       contact
+    });
+}
+
+export default {
+    getAll: cntrlWrapper(getAll),
+    getById: cntrlWrapper(getById),
+    add: cntrlWrapper(add),
+    deleteById: cntrlWrapper(deleteById),
+    updateById: cntrlWrapper(updateById)
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,86 +1,16 @@
-import { nanoid } from "nanoid";
 import express from 'express'
-import * as contactService from "../../models/contacts.js"
-import HttpError from "../../helpers/HttpError.js";
+import contactsController from "../../controllers/contacts-controller.js";
 
 const contactsRouter = express.Router()
 
-const notFound = () => {
-  return res.status(404).json({
-    message: "Not found"
-  })
-}
+contactsRouter.get('/', contactsController.getAll);
 
+contactsRouter.get('/:id', contactsController.getById);
 
-const checkBody = ({ name = null, email = null, phone = null }) => {
-  return `${!name ? 'name': ''}${!email ? ' email': ''}${!phone ? ' phone': ''}`.trim().replaceAll(' ', ', ')
-}
+contactsRouter.post('/', contactsController.add);
 
+contactsRouter.delete('/:id', contactsController.deleteById);
 
-contactsRouter.get('/', async (req, res, next) => {
-  try {
-    const contacts = await contactService.listContacts();
-    res.status(200).json({
-        contacts
-    })
-  } catch (error) {
-     next(error)
-  }
-})
-
-contactsRouter.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const contact = await contactService.getContactById(id)
-    if (!contact) throw HttpError(404, "Not found")
-    res.status(200).json({
-      contact 
-    })
-  } catch (error) {
-    next(error)
-  }
-})
-
-contactsRouter.post('/', async (req, res, next) => {
-  try {
-    const messageBody = checkBody(req.body)
-    if (messageBody.length>0) throw HttpError (400, `missing required ${messageBody} field`)
-    const contact = await contactService.addContact({ id: nanoid(), ...req.body })
-    res.status(201).json({
-      contact 
-    });
-  } catch (error) {
-    next(error)
-  }
-})
-
-contactsRouter.delete('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const contact = await contactService.removeContact(id)
-    if (!contact) throw HttpError(404, "Not found");
-    res.status(200).json({
-      message: "contact deleted",
-       contact
-    })
-  } catch (error) {
-    next (error)
-  }
-})
-
-contactsRouter.put('/:id', async (req, res, next) => {
-  try {
-    if (Object.keys(req.body).length === 0) throw HttpError(400, `missing fields`);
-    const { id } = req.params;
-    const contact = await contactService.updateContact(id, req.body);
-    if (!contact) throw HttpError(404, "Not found");
-    res.status(200).json({
-       contact
-    });
-  } catch (error) {
-    next(error)
-  }
-  
-})
+contactsRouter.put('/:id', contactsController.updateById);
 
 export default contactsRouter;

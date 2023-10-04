44708feb64081a91bdcd691c06698ca7ddbb5c3a
Dmytro-Lys <d.lysenko122@gmail.com>
2023-10-04 14:41:22 +0300

    clarification

--- middlewares/isEmptyBody.js
+++ middlewares/isEmptyBody.js
@@ -2,7 +2,7 @@ import { HttpError } from "../helpers/index.js";
 
 const isEmptyBody = (req, res, next) => {
     if (!Object.keys(req.body).length) {
-        return next(HttpError(400, `missing field${req.path.endsWith("favorite") ? " favorite": "s"}` ));
+        return next(HttpError(400, `missing field${req.path.endsWith("/favorite") ? " favorite": "s"}` ));
     }
     next();
 }
